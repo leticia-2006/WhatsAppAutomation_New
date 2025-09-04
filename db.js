591b8284f1bd551f4b7cde78a67ef264c6836fd7
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-09-05 00:38:03 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');
 
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '-hc(m5dSUH2t^',
-    database: 'whatsapp_automation',
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: pprocess.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
 });
 
-module.exports = db;
+module.exports = pool;
+
 
