b5f1ae62ce125de5df873be320459cd535113fef
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-09-05 01:10:00 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     host: process.env.DB_HOST,
-    user: pprocess.env.DB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
@@ -10,5 +10,6 @@ const pool = new Pool({
 });
 
 module.exports = pool;
+
 
 
