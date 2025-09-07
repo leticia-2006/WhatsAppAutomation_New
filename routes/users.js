4217cf04b5aa8c582223a26b1eb5cb1a247886db
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-09-07 12:38:11 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,18 +6,17 @@ const { requireLogin, checkRole } = require('../middleware')
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = 'mysecretkey';
 
+
 // إضافة Agent
-router.post('/add-agent', requireLogin,checkRole('super_admin'), async (req, res) => {
+router.post('/add-agent', requireLogin, checkRole('super_admin'), async (req, res) => {
     try {
         const { name, phone, password } = req.body;
-        console.log("Password from input:",password);
-        console.log("Hashed password from DB:",user.password);
-        const match = await bcrypt.compare(password, user.password);
+        const hashedPassword = await bcrypt.hash(password, 10);
         await db.query(
             "INSERT INTO users (name, phone, role, password) VALUES ($1, $2, 'agent', $3)",
             [name, phone, hashedPassword]
         );
-        res.json({ message: 'Agent added successfully' });
+        res.json({ "message": "Agent added successfully" });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
@@ -80,6 +79,7 @@ router.post('/login', async (req, res) => {
             console.log("Password did not match for user:", user.name);
             return res.status(401).json({ message: 'Invalid credentials' });
         }
+        req.session.user = { id: user.id, name: user.name, role: user.role };
        
         const token = jwt.sign(
         { id: user.id, name: user.name, role: user.role },
@@ -95,6 +95,7 @@ router.post('/login', async (req, res) => {
 
 
 module.exports = router;
+
 
 
 
