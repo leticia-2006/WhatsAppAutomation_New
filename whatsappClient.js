37aba39242ced344deaa231c7b94a0d444fb10b5
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-09-07 14:21:16 +0100

    Update whatsappClient.js

--- whatsappClient.js
+++ whatsappClient.js
@@ -4,36 +4,42 @@ const qrcode = require('qrcode-terminal');
 let latestQR = {};
 
 // تفعيل LocalAuth لحفظ الجلسات على السيرفر
+function createWhatsAppClient(clientId) {
 const client = new Client({
-    authStrategy: new LocalAuth({
-        clientId: "client1" // يمكن إضافة clientId لكل رقم
-    }),
-    puppeteer: { headless: true }
-});
+    authStrategy: new LocalAuth({clientId }),
+    puppeteer: { headless: true }}); // يمكن إضافة clientId لكل رقم
+}),
 
 // حدث توليد QR Code عند إضافة رقم جديد فقط
 client.on('qr', (qr) => {
-    const currentNumberId = 'client1';
-    latestQRs[currentNumberId] = qr;
-    console.log('QR Code generated for', currentNumberId);
+    latestQRs[clientId] = qr;
+    console.log('QR Code generated for ${clientId}`);
     qrcode.generate(qr, { small: true }); 
 });
 
 // حدث الجلسة جاهزة
 client.on('ready', () => {
-    console.log('WhatsApp Client is ready!');
+    console.log(`WhatsApp Client ${clientId} is ready!`);
 });
 
 // فشل المصادقة
 client.on('auth-failure', msg => {
-    console.error('Authentication failure:', msg);
+    console.error('Authentication failurfor ${clientId}:`, msg);
 });
 
 // فصل الاتصال
 client.on('disconnected', reason => {
-    console.log('Client disconnected:',reason);
+    console.log(`Client ${clientd} disconnected:`,reason);
 });
 
+client.initialize();
+return client;
+}
+
+function getLatestQR(clientId) {
+    return latestQRs[clientId] || null;
+}
+const client1 = createWhatsAppClient("client1")
 // بدء الجلسة
 client.initialize();
 
