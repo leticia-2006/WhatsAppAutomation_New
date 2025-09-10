64277004e2895ba51b2caa343b8fb7846a0a70f2
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-09-10 14:49:19 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,12 +1,20 @@
 function requireLogin(req, res, next) {
-    if (!req.session.user) return res.status(401).json({ message: 'Login required' });
+    if (!req.session || !req.session.user)
+    { return res.status(401).json({ message: 'Login required' });
     next();
 }
 
 function checkRole(role) {
     return (req, res, next) => {
-        if (req.session.user.role === role || req.session.user.role === 'super_admin') next();
-        else res.status(403).json({ message: 'Access denied' });
+        const userRole= req.session.user.role;
+        if (userRole === 'super_admin') {
+            return  next();
+        }
+        if (roles.inculdes(userRole)) {
+                return next();
+            }
+        return
+res.status(403).json({ message: 'Access denied' });
     };
 }
 
