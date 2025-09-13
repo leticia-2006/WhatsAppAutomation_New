67880f3662a6a499e64fa74bde0cf23b1ecece26
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-09-13 02:55:41 +0100

    Update clients.js

--- routes/clients.js
+++ routes/clients.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../db.js');
+const db = require('../db');
 const { requireLogin } = require('../middleware/auth');
 
 router.use(requireLogin);
@@ -39,6 +39,7 @@ router.get('/:client_id', async (req, res) => {
 });
 
 module.exports = router;
+
 
 
 
