dd0c441fa74d9b76a277d48075455c5db9d817ef
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-09-14 19:16:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session({
 
 app.use((req, res, next) => {
     res.setHeader("Content-Security-Policy","default-src * 'unsafe-inline' 'unsafe-eval'");
- console.log("Cookies from client:",
+ console.log("Cookies from client:", req.headers.cookie);
     next();
 });
 
@@ -68,6 +68,7 @@ server.listen(PORT, '0.0.0.0', () => {
 });
 
 module.exports = server;
+
 
 
 
