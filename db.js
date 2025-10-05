b2f922e357cc36a41ee936227f3258f49083a82d
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-10-05 05:23:10 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: "postgresql://neondb_owner:npg_wkKIQ5RGpxz8@ep-patient-haze-a8pglah9-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require",
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
 });
 pool.on("error", (err) => {
   console.error("Unexpected DB error:", err);
@@ -10,6 +10,7 @@ pool.on("error", (err) => {
 });
 
 module.exports = pool;
+
 
 
 
