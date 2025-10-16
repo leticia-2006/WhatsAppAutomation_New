201af7fbe38394008b49d8339059ecc4bd1360db
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-10-16 04:53:19 +0100

    Update sessions.js

--- frontend/sessions.js
+++ frontend/sessions.js
@@ -124,6 +124,7 @@ function renderSessions(list = [], filterType = "all") {
         <div class="client-message">${session.last_message ? session.last_message.slice(0, 30) + "…" : "No messages yet"}</div>
         </div>
         <small class="client-time">${session.last_time || ""}</small>
+        </div>
         <div class="client-status ${session.is_online ? "online" : "offline"}"></div>
       
       <div class="client-tags">
