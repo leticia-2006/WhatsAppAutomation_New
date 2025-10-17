e26d8794368feccb6773bae9f1eec48cd6e7afc4
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-10-17 14:25:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ console.log("Server file started running...");
 
 // Middleware
 app.use(cors({
-  origin: [ "https://whatsappautomation-new-4fec.onrender.com", "http://localhost:3000" ],
+  origin: [ "https://chat.ohgo.site", "http://localhost:3000" ],
   credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -139,6 +139,7 @@ process.on("unhandledRejection", (reason, promise) => {
 console.error("Unhandled Rejection:", reason);
 });
 module.exports = server;
+
 
 
 
