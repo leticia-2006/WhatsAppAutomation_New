35e28ecb676cc9ec0a83c647ea53086885d80476
leticia-2006 <leticiabouaiche2025@gmail.com>
2025-12-26 00:24:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -131,15 +131,16 @@ const server = http.createServer(app);
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
-/*reconnectAllActive().then(() => {
+reconnectAllActive().then(() => {
   console.log("✅ تم إعادة تشغيل جميع الأرقام النشطة");
-});*/process.on("uncaughtException", (err) => {
+});process.on("uncaughtException", (err) => {
  console.error("Uncaught Exception:", err);
 });
 process.on("unhandledRejection", (reason, promise) => {
 console.error("Unhandled Rejection:", reason);
 });
 module.exports = server;
+
 
 
 
